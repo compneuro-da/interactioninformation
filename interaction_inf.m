8780ee39bab4920482d2343c53f655ad190e276a
Daniele Marinazzo <daniele.marinazzo@gmail.com>
2018-03-17 18:56:30 +0100

    second commit

--- interaction_inf.m
+++ interaction_inf.m
@@ -1,26 +1,27 @@
-function h = interaction_inf(vec1,vec2,condvec,th)
+function [h, h0] = interaction_inf(vec1,vec2,condvec,th)
 
+nperm=1000; % number of permutations
 
 h0 =condmutualinfo(vec1,vec2,condvec)-mutualinfo(vec1,vec2);
 n=length(vec1);
 ncont=0;
 
 if h0 >0
-    for j=1:1000
+    for j=1:nperm
         vec1=vec1(randperm(n));
         h=condmutualinfo(vec1,vec2,condvec)-mutualinfo(vec1,vec2);
         if h>h0
             ncont=ncont+1;
         end
     end
-    if ncont >th*1000
+    if ncont >th*nperm
         h=0;
     else
         h=1;
     end
 else
     
-    for j=1:1000
+    for j=1:nperm
         vec1=vec1(randperm(n));
         h=condmutualinfo(vec1,vec2,condvec)-mutualinfo(vec1,vec2);
         if h<h0
@@ -28,7 +29,7 @@ else
         end
     end
     
-    if ncont >th*1000
+    if ncont >th*nperm
         h=0;
     else
         h=-1;
--- main_interaction_information.m
+++ main_interaction_information.m
@@ -2,6 +2,7 @@ clear;clc;
 %%% HERE LOAD YOUR DATA, CALL IT mydata %%%%%%%%%%
 [npoints, n]=size(mydata); %make sure that the variables are the 2nd dimension
 th=0.05/(n*(n-1)*0.5); % threshold with Bonferroni
+muti=zeros(n);
 for i=1:n
     for j=i+1:n
         muti(i,j)=mutualinfos(mydata(:,i),mydata(:,j),th); %mutual info with threshold
@@ -11,12 +12,32 @@ end
 
 th=0.05*6/(n*(n-1)*(n-2)); %threshold for triplets with Bonferroni
 
-h=0;
+%%% now build the 3D matrix of II values, plus a list of red, syn,
+%%% independent triplets
+II_tot=zeros(n,n,n);
+Ind_red=0;
+Ind_syn=0;
+Ind_ind=0;
 for i=1:n
     for j=i+1:n
         for k=j+1:n
-            h=h+1;
-            tripl(h)=interaction_inf(mydata(:,i),mydata(:,j),mydata(:,k),th);
+            [Itest, II]=interaction_inf(mydata(:,i),mydata(:,j),mydata(:,k),th);
+            II_tot(i,j,k)=II;
+            II_tot(i,k,j)=II;
+            II_tot(j,i,k)=II;
+            II_tot(j,k,i)=II;
+            II_tot(k,i,j)=II;
+            II_tot(k,j,i)=II;
+            if Itest>0
+                Ind_syn=Ind_syn+1;
+                list_syn(Ind_syn)=mat2str([i,j,k]);
+            elseif Itest<0
+                Ind_red=Ind_red+1;
+                list_red(Ind_red)=mat2str([i,j,k]);
+            else
+                Ind_ind=Ind_ind+1;
+                list_ind(Ind_ind)=mat2str([i,j,k]);
+            end
         end
     end
 end
